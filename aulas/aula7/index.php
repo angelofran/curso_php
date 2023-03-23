367545b5e04da98c1971f1c45cb3b359bf46118c
Yuky オ <90131020+angelofran@users.noreply.github.com>
2023-03-23 11:56:14 +0100

    Updated

--- aulas/aula7/index.php
+++ aulas/aula7/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 7</title>
+    <title>Aula 7 - Operadores aritiméticos</title>
 </head>
 <body>
     
